54e9f9813c2d2df8c3bb0bc7371f5f6e32958284
peppe-p <ombrachiara96@gmail.com>
2021-07-21 19:25:02 +0200

    Fix route from home to admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         return view('admin.home');
---
+++ resources/views/layouts/structure.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>BoolPress</title>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body>
+    <div class="container">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ route('home') }}">Profilo</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+
+    @yield('content')
+</body>
+
+</html>
